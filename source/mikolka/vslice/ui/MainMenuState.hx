d0835c5f11dc0f1d6431ea8582b94f41497f598a
mingokrb <82242100+mingokrb@users.noreply.github.com>
2025-07-09 01:27:38 -0300

    menu principal 3.5: esqueci

--- source/mikolka/vslice/ui/MainMenuState.hx
+++ source/mikolka/vslice/ui/MainMenuState.hx
@@ -59,7 +59,7 @@ class MainMenuState extends MusicBeatState
 		persistentUpdate = persistentDraw = true;
 
 		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
-		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('mainmenu/menuBG'));
 		bg.antialiasing = false;
 		bg.scrollFactor.set();
 		//bg.setGraphicSize(Std.int(bg.width * 1.175));
